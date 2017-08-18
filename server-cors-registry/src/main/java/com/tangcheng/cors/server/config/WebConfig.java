48ed2a621d38b4d23b0f6cae04c5ee332dab4960
helloworld.tang <helloworld.tang@qq.com>
2017-08-18 16:31:01 +0800

    add comment

--- server-cors-registry/src/main/java/com/tangcheng/cors/server/config/WebConfig.java
+++ server-cors-registry/src/main/java/com/tangcheng/cors/server/config/WebConfig.java
@@ -11,7 +11,24 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+
+        /**
+         * Enable cross-origin request handling for the specified path pattern.
+         * <p>Exact path mapping URIs (such as {@code "/admin"}) are supported as
+         * well as Ant-style path patterns (such as {@code "/admin/**"}).
+         * <p>By default, all origins, all headers, credentials and {@code GET},
+         * {@code HEAD}, and {@code POST} methods are allowed, and the max age
+         * is set to 30 minutes.
+         * @param pathPattern the path pattern to enable CORS handling for
+         * @return CorsRegistration the corresponding registration object,
+         * allowing for further fine-tuning
+         */
         registry.addMapping("/**")
+                /**
+                 * Set the origins to allow, e.g. {@code "http://domain1.com"}.
+                 * <p>The special value {@code "*"} allows all domains.
+                 * <p>By default, all origins are allowed.
+                 */
                 .allowedOrigins("http://localhost")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
